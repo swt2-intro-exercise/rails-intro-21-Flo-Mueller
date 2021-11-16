964505567fb5ab8b58fc3c18d600e21403f18c3d
Flo-Mueller <florian.mueller141@web.de>
2021-11-16 21:18:32 +0100

    fixed test #07

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -1,9 +1,8 @@
 describe "Index Author Page", type: :feature do
     it "should show all Authors" do
       visit authors_url
-      expect(page).to have_table 'Authors'
-      expect('Authors').to have_header 'Name'
-      expect('Authors').to have_header 'Homepage'
+      expect(page).to have_text 'Alan Turing'
+      expect(page).to have_text 'https://en.wikipedia.org/wiki/Alan_Turing'
       expect(page).to have_link 'New', href: new_author_path
     end
 end
