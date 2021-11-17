471cad7e95a7f597690ece6c94b38634b66f6ea3
Flo-Mueller <florian.mueller141@web.de>
2021-11-17 20:21:13 +0100

    JETZT ABER IHR

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Index Author Page", type: :feature do
     it "should show all Authors" do
-      visit authors_url
+      visit authors_path
       expect(page).to have_text 'Alan'
       expect(page).to have_text 'Turing'
       expect(page).to have_text 'https://en.wikipedia.org/wiki/Alan_Turing'
