747b21db65cb0b9fb24fe1bafdf348ee26d5ab71
Flo-Mueller <florian.mueller141@web.de>
2021-11-17 20:13:01 +0100

    split test

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -1,9 +1,9 @@
 describe "Index Author Page", type: :feature do
     it "should show all Authors" do
       visit authors_url
-      expect(page).to have_text 'Alan Turing'
+      expect(page).to have_text 'Alan'
+      expect(page).to have_text 'Turing'
       expect(page).to have_text 'https://en.wikipedia.org/wiki/Alan_Turing'
       expect(page).to have_link 'New', href: new_author_path
-      
     end
 end
