d7a0143db80c9267b8115b7fca28122904d0772d
Flo-Mueller <florian.mueller141@web.de>
2021-11-17 20:23:29 +0100

    ICH WERD WILD

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-describe "Index Author Page", type: :feature do
+describe "Authors index view", type: :feature do
     it "should show all Authors" do
       visit authors_path
-      expect(page).to have_text 'Alan'
-      expect(page).to have_text 'Turing'
-      expect(page).to have_text 'https://en.wikipedia.org/wiki/Alan_Turing'
+      expect(page).to have_text "Name"
+      expect(page).to have_text "Homepage"
       expect(page).to have_link 'New', href: new_author_path
     end
 end
