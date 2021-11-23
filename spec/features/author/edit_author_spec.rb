f4115390b64127a35c82667d0e54592c4d09c9ae
Flo-Mueller <florian.mueller141@web.de>
2021-11-23 20:39:44 +0100

    Fixed failing test for #10

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 describe "Edit author page", type: :feature do
     it "should show no errors" do
-        visit edit_author_path
+        @alan = FactoryBot.create :author
+        visit edit_author_path(@alan)
         # these are the standard names given to inputs by the Rails form builder
         expect(page).to not_have("error")
+    end
+    it "should update the data" do
+        @author = create(:author)
+        visit edit_author_path(@author)
+        fill_in "author[first_name]", with: "Albert"
+        fill_in "author[last_name]", with: "Einstein"
+        fill_in "author[homepage]", with: "https://de.wikipedia.org/wiki/Albert_Einstein"
+        click_button "Update Author"
+        @author.reload
+        expect(author.first_name).to eq("Albert")
+        expect(author.last_name).to eq("Einstein")
+        expect(author.homepage).to eq("https://de.wikipedia.org/wiki/Albert_Einstein")
     end
 end
