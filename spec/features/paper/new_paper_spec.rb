a9e865d3d7fbef2b640396c915c80ee359c04b83
Flo-Mueller <florian.mueller141@web.de>
2021-11-25 01:00:00 +0100

    Failing Test #14

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -8,4 +8,13 @@ describe "New paper page", type: :feature do
         expect(page).to have_field('paper[venue]')
         expect(page).to have_field('paper[year]')
     end
+    it "should not be valid if any value is nil" do
+        visit new_paper_path
+        @papier = Paper.new(title: "Party", venue: nil, year: 1984)
+        expect(@papier).to_not be_valid
+        @papier2 = Paper.new(title: nil, venue: "Mercedes-Benz Arena", year: 1984)
+        expect(@papier2).to_not be_valid
+        @papier2 = Paper.new(title: "Party", venue: "Mercedes-Benz Arena", year: '12')
+        expect(@papier2).to_not be_valid
+    end
 end
