f2e0496083ee5243b8926138c2ff0eb09d901e2c
Flo-Mueller <florian.mueller141@web.de>
2021-11-25 10:09:18 +0100

    Failing Test #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have an empty author_list created" do
+    @maz =Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(@maz.authors).to be_valid
+  end
 end
